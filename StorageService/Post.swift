87b93be0edf5127f5fea1888ff078d3209aa9bee
Alex S.K <98609838+strel4k@users.noreply.github.com>
2022-08-12 16:33:06 +0300

    FW

---
+++ StorageService/Post.swift
@@ -0,0 +1,28 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Alexander on 24.05.2022.
+//
+
+import UIKit
+
+public struct somePost {
+    
+    public var author: String
+    public var title: String
+    public var description: String
+    public var image: String
+    public var likes: Int
+    public var views: Int
+}
+    
+//let post_1 = Post(author: "bruce.official", title: "Yeeeeeaaah", description: "We already have a 1 000 000", image: "post_1", likes: 96928, views: 600000)
+//
+//let post_2 = Post(author: "bbc.news", title: "Sunshine", description: "New photo of the Sun", image: "post_2", likes: 21312, views: 32123)
+//
+//let post_3 = Post(author: "oskar.fun", title: "my 2022", description: "Hello from WS", image: "post_3", likes: 123123, views: 332123)
+//
+//let post_4 = Post(author: "reddit", title: "Morbius is here", description: "Jared Leto on grammy awards", image: "post_4", likes: 5621312, views: 7732123)
+//
+//public var posts: [Post] = [post_1, post_2, post_3, post_4]
